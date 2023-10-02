a14b5182b5ad0515cdd7aa59d114feab1a5fdcf2
KWottrich <kenny.wottrich@gmail.com>
2023-10-01 21:02:54 -0500

    Print output report requests received

--- ally-steam-controller.c
+++ ally-steam-controller.c
@@ -21,53 +21,6 @@
 #define VENDOR_ID 0x28de
 #define PRODUCT_ID 0x1205
 
-static unsigned char rdesc[] = {
-	0x05, 0x01,	/* USAGE_PAGE (Generic Desktop) */
-	0x09, 0x02,	/* USAGE (Mouse) */
-	0xa1, 0x01,	/* COLLECTION (Application) */
-	0x09, 0x01,		/* USAGE (Pointer) */
-	0xa1, 0x00,		/* COLLECTION (Physical) */
-	0x85, 0x01,			/* REPORT_ID (1) */
-	0x05, 0x09,			/* USAGE_PAGE (Button) */
-	0x19, 0x01,			/* USAGE_MINIMUM (Button 1) */
-	0x29, 0x03,			/* USAGE_MAXIMUM (Button 3) */
-	0x15, 0x00,			/* LOGICAL_MINIMUM (0) */
-	0x25, 0x01,			/* LOGICAL_MAXIMUM (1) */
-	0x95, 0x03,			/* REPORT_COUNT (3) */
-	0x75, 0x01,			/* REPORT_SIZE (1) */
-	0x81, 0x02,			/* INPUT (Data,Var,Abs) */
-	0x95, 0x01,			/* REPORT_COUNT (1) */
-	0x75, 0x05,			/* REPORT_SIZE (5) */
-	0x81, 0x01,			/* INPUT (Cnst,Var,Abs) */
-	0x05, 0x01,			/* USAGE_PAGE (Generic Desktop) */
-	0x09, 0x30,			/* USAGE (X) */
-	0x09, 0x31,			/* USAGE (Y) */
-	0x09, 0x38,			/* USAGE (WHEEL) */
-	0x15, 0x81,			/* LOGICAL_MINIMUM (-127) */
-	0x25, 0x7f,			/* LOGICAL_MAXIMUM (127) */
-	0x75, 0x08,			/* REPORT_SIZE (8) */
-	0x95, 0x03,			/* REPORT_COUNT (3) */
-	0x81, 0x06,			/* INPUT (Data,Var,Rel) */
-	0xc0,			/* END_COLLECTION */
-	0xc0,		/* END_COLLECTION */
-	0x05, 0x01,	/* USAGE_PAGE (Generic Desktop) */
-	0x09, 0x06,	/* USAGE (Keyboard) */
-	0xa1, 0x01,	/* COLLECTION (Application) */
-	0x85, 0x02,		/* REPORT_ID (2) */
-	0x05, 0x08,		/* USAGE_PAGE (Led) */
-	0x19, 0x01,		/* USAGE_MINIMUM (1) */
-	0x29, 0x03,		/* USAGE_MAXIMUM (3) */
-	0x15, 0x00,		/* LOGICAL_MINIMUM (0) */
-	0x25, 0x01,		/* LOGICAL_MAXIMUM (1) */
-	0x95, 0x03,		/* REPORT_COUNT (3) */
-	0x75, 0x01,		/* REPORT_SIZE (1) */
-	0x91, 0x02,		/* Output (Data,Var,Abs) */
-	0x95, 0x01,		/* REPORT_COUNT (1) */
-	0x75, 0x05,		/* REPORT_SIZE (5) */
-	0x91, 0x01,		/* Output (Cnst,Var,Abs) */
-	0xc0,		/* END_COLLECTION */
-};
-
 static unsigned char reportDescriptor[] = {
 	0x06, 0xFF, 0xFF,  // Usage Page (Vendor Defined 0xFFFF)
 	0x09, 0x01,        // Usage (0x01)
@@ -134,19 +87,9 @@ static void destroy(int fd)
  * print debug messages for it. */
 static void handle_output(struct uhid_event *ev)
 {
-	/* LED messages are adverised via OUTPUT reports; ignore the rest */
-	if (ev->u.output.rtype != UHID_OUTPUT_REPORT)
-		return;
-	/* LED reports have length 2 bytes */
-	if (ev->u.output.size != 2)
-		return;
-	/* first byte is report-id which is 0x02 for LEDs in our rdesc */
-	if (ev->u.output.data[0] != 0x2)
-		return;
-
-	/* print flags payload */
-	fprintf(stderr, "LED output report received with flags %x\n",
+	fprintf(stdout, "Kernel sent output report of type 0x%x\n",
 		ev->u.output.data[1]);
+	return;
 }
 
 static int event(int fd)
